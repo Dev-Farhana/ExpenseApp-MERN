cffa68230fbe7295fd0bff43bd9ac8cc57808e79
DevFarhana <124885288+Dev-Farhana@users.noreply.github.com>
2024-04-17 20:32:08 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 const dotenv = require("dotenv");
 const connectDb = require("./config/connectDb");
 dotenv.config();
@@ -13,6 +14,13 @@ app.use(morgan("dev"));
 
 app.use(cors());  
 app.use(express.json());
+
+app.use(express.static(path.resolve("./client/dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./client/dist/index.html"));
+});
+
 
 //for user routes;
 app.use("/api/v1/users", require("./routes/userRoute"));
